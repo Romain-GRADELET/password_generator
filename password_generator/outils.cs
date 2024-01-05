f2b2997614fb12d2511d9086fb6e27336828af1b
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-05 11:06:35 +0100

    generated password with only lowercase

---
+++ password_generator/outils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            /*int nombre = DemanderNombre(question);
+            if (nombre > 0) 
+            {
+                return nombre;
+            }
+            Console.WriteLine("ERREUR: Veuillez renseigner un nombre supérieur à 0");
+
+            return DemanderNombrePositifNonNul(question);*/
+
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+            while (true)
+            {
+                int nombre = DemanderNombre(question);
+                if ((nombre >= min) && (nombre <= max))
+                {
+                    // Valide
+                    return nombre;
+                }
+                Console.WriteLine("ERREUR: Le nombre doit être compris entre " + min + " et " + max);
+            }
+
+        }
+
+        public static int DemanderNombre(string question)
+        {
+
+            while (true)
+            {
+                // Poser la question
+                Console.Write(question);
+                // Récupérer la réponse
+                string reponse = Console.ReadLine();
+                // Convertir
+                // Gérer l'erreur de conversion
+                try
+                {
+                    int passwordLength = int.Parse(reponse);
+                    return passwordLength;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR: Veuillez rentrer un nombre valide");
+                }
+                // Boucler tant que l'on a pas reçu une réponse valide (qui contient que des chiffres)
+            }
+        }
+
+
+    }
+}
