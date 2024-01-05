042953f7164b07d4f7bd4b6d65bde8a493f75874
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-05 09:32:00 +0100

    RoadMap

--- password_generator/Program.cs
+++ password_generator/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
-namespace nombre_magique
+namespace generateur_mot_de_passe
 {
     class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            // 1 - Demander la longueur du mot de passe (Delmander nombre) int longuieur mot de passe = ...
+            // 2 - Alphabet "abcd1234..."
+            // 3 - Comment choisir un caractère aléatoire
+            // 4 - Générer le mot de passe
+            // 5 - Améliorations : Choix alphabet
+
         }
     }
 }
